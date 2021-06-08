170601c254e2b0879ddb6e550bd8ad758ad7080c
Andre Melo <andrevsm@Andres-MacBook-Air.local>
2021-06-08 19:42:51 -0300

    Added navigation to the fragments

--- app/src/main/java/com/avsm/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/avsm/android/navigation/TitleFragment.kt
@@ -19,9 +19,8 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false)
 
-        binding.playButton.setOnClickListener {
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener
+            (R.id.action_titleFragment_to_gameFragment))
 
         return binding.root
     }
